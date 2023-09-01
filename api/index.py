298a45c7d379c64f8047fcfad58fba0ad161f361
varundubey771 <49694579+varundubey771@users.noreply.github.com>
2023-09-01 16:42:50 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -78,7 +78,7 @@ def getJobData():
 
 @app.route('/sendJobDataCron')
 def sendJobDataCron():
-   countries = ["United Kingdom","Ireland","Germany"]
+   countries = ["United Kingdom","Ireland"]
    jobs = ["Python", "Java", "Data Science"]
    lst=[]
    print(countries)
@@ -97,7 +97,7 @@ def sendJobDataCron():
 
 @app.route('/sendJobDataCronPartition2')
 def sendJobDataCronPartition2():
-   countries = ["Australia","India","United States"]
+   countries = ["Australia","India"]
    jobs = ["Python", "Java", "Data Science"]
    lst=[]
    print(countries)
@@ -112,3 +112,21 @@ def sendJobDataCronPartition2():
    print(lst)
    resp = (writer(lst))
    return lst   
+
+@app.route('/sendJobDataCronPartition3')
+def sendJobDataCronPartition2():
+   countries = ["Germany","United States"]
+   jobs = ["Python", "Java", "Data Science"]
+   lst=[]
+   print(countries)
+   print(jobs)
+   current_datetime = datetime.datetime.now()
+   str_cur_dt= current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+   parsed_datetime = datetime.datetime.strptime(str_cur_dt, "%Y-%m-%d %H:%M:%S")
+   for country in countries:
+       for job in jobs:
+           job_count = int(scrape_jobs(country,job))
+           lst.append((country, job_count, job,parsed_datetime))
+   print(lst)
+   resp = (writer(lst))
+   return lst       
